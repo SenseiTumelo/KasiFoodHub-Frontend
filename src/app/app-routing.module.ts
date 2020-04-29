ba18a466c2897bde3464a656485af31c871e32d2
nicolasmahlangu75 <nicolasmahlangu75@gmail.com>
2020-04-29 22:09:34 +0300

    Nicholas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -119,7 +119,11 @@ const routes: Routes = [
   {
     path: 'reset-password',
     loadChildren: () => import('./pages/reset-password/reset-password.module').then( m => m.ResetPasswordPageModule)
+  },  {
+    path: 'menu',
+    loadChildren: () => import('./pages/vendor/menu/menu.module').then( m => m.MenuPageModule)
   },
+
 
 ];
 @NgModule({
