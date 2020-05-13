b189c51ef0655ac1ce6f673e0353bf0f6a558716
nicolasmahlangu75 <nicolasmahlangu75@gmail.com>
2020-05-13 22:26:39 +0300

    Nicholas

--- server_api/library/config.php
+++ server_api/library/config.php
@@ -3,13 +3,9 @@
     define('DB_NAME','kasi_database');
     define('DB_USER','root');
     define('DB_PASSWORD','');
-    define('DB_HOST','localhost');
-
-
+    define('DB_HOST','loscalhost');
 
     $mysqli = new mysqli(DB_HOST,DB_USER,DB_PASSWORD,DB_NAME);
 
-
-     /*mysqli_connect('localhost','root','','kasi_database');
-*/
+     
 ?>
