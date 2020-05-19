f679b44ce1652b9da9f51e98bd44050c7161463e
senseitumelo <tmotjopi@gmail.com>
2020-05-19 05:49:58 +0200

    ....

--- src/app/components/start/start.component.ts
+++ src/app/components/start/start.component.ts
@@ -13,7 +13,7 @@ export class StartComponent implements OnInit {
   ngOnInit() {}
 
   navigateToLoginPage() {
-    this.router.navigateByUrl('/home');
+    this.router.navigateByUrl('/signin');
   }
 
 }
