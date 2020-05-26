82f8cdbda5c1d17fb0b1b57792445121cc278940
ashoktut <ashoktut.2@gmail.com>
2020-05-26 03:59:10 +0200

    super admin 1.4

--- src/app/pages/super-admin/super-admin.page.ts
+++ src/app/pages/super-admin/super-admin.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { IonSlides } from '@ionic/angular';
 
 @Component({
   selector: 'app-super-admin',
@@ -6,10 +7,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./super-admin.page.scss'],
 })
 export class SuperAdminPage implements OnInit {
+  segment = 0;
+  selectedSlide: any;
+  sliderOptions = {
+    initialSlide : 0,
+    slidesPerView: 1,
+    speed: 400
+  }
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  async segmentChanged(){
+    await this.selectedSlide.slideTo(this.segment);
+
+  }
+
+  async slideChanged(slides : IonSlides){
+    this.selectedSlide = slides;
+    slides.getActiveIndex().then(selectedIndex =>{
+      this.segment = selectedIndex;
+    })
   }
 
 }
