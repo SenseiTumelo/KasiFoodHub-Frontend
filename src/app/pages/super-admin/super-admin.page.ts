e97e018e573cbd537b142cdc33144253fc7723cd
ashoktut <ashoktut.2@gmail.com>
2020-07-16 14:11:12 +0200

    new

--- src/app/pages/super-admin/super-admin.page.ts
+++ src/app/pages/super-admin/super-admin.page.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import { IonSlides } from '@ionic/angular';
+import { Chart } from 'chart.js';
 
 @Component({
   selector: 'app-super-admin',
@@ -7,6 +8,11 @@ import { IonSlides } from '@ionic/angular';
   styleUrls: ['./super-admin.page.scss'],
 })
 export class SuperAdminPage implements OnInit {
+  doughnutChart: any;
+  lineChart: any;
+
+
+  constructor() { }
   segment = 0;
   selectedSlide: any;
   sliderOptions = {
@@ -15,7 +21,15 @@ export class SuperAdminPage implements OnInit {
     speed: 400,
   };
 
+
   items = ['apple', 'banana', 'cherry', 'apple', 'banana', 'cherry'];
+
+  @ViewChild('barCanvas',  { static: true }) barCanvas: ElementRef;
+  @ViewChild('doughnutCanvas',  { static: true }) doughnutCanvas: ElementRef;
+  @ViewChild('lineCanvas',  { static: true }) lineCanvas: ElementRef;
+
+
+  private barChart: Chart;
 
 
   unread() {
@@ -31,9 +45,100 @@ export class SuperAdminPage implements OnInit {
     console.log('my delete works');
   }
 
-  constructor() { }
-
   ngOnInit() {
+    this.barChart = new Chart(this.barCanvas.nativeElement, {
+      type: 'bar',
+      data: {
+        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+        datasets: [
+          {
+            label: '# of Votes',
+            data: [12, 19, 3, 5, 2, 3],
+            backgroundColor: [
+              'rgba(255, 99, 132, 0.2)',
+              'rgba(54, 162, 235, 0.2)',
+              'rgba(255, 206, 86, 0.2)',
+              'rgba(75, 192, 192, 0.2)',
+              'rgba(153, 102, 255, 0.2)',
+              'rgba(255, 159, 64, 0.2)'
+            ],
+            borderColor: [
+              'rgba(255,99,132,1)',
+              'rgba(54, 162, 235, 1)',
+              'rgba(255, 206, 86, 1)',
+              'rgba(75, 192, 192, 1)',
+              'rgba(153, 102, 255, 1)',
+              'rgba(255, 159, 64, 1)'
+            ],
+            borderWidth: 1
+          }
+        ]
+      },
+      options: {
+        scales: {
+          yAxes: [
+            {
+              ticks: {
+                beginAtZero: true
+              }
+            }
+          ]
+        }
+      }
+    });
+
+    this.doughnutChart = new Chart(this.doughnutCanvas.nativeElement, {
+      type: 'doughnut',
+      data: {
+        labels: ['Spathlo', 'Skopo', 'Pap And Wors', 'Pap', 'Marapo', 'Mogodu'],
+        datasets: [
+          {
+            label: '# of Votes',
+            data: [12, 19, 3, 5, 2, 3],
+            backgroundColor: [
+              'rgba(255, 99, 132, 0.2)',
+              'rgba(54, 162, 235, 0.2)',
+              'rgba(255, 206, 86, 0.2)',
+              'rgba(75, 192, 192, 0.2)',
+              'rgba(153, 102, 255, 0.2)',
+              'rgba(255, 159, 64, 0.2)'
+            ],
+            hoverBackgroundColor: ['#FF6384', '#36A2EB', '#FFCE56', '#FF6384', '#36A2EB', '#FFCE56']
+          }
+        ]
+      }
+    });
+
+    this.lineChart = new Chart(this.lineCanvas.nativeElement, {
+      type: 'line',
+      data: {
+        labels: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec', ],
+        datasets: [
+          {
+            label: 'Annual Business Performance',
+            fill: false,
+            lineTension: 0.1,
+            backgroundColor: 'rgba(75,192,192,0.4)',
+            borderColor: 'rgba(75,192,192,1)',
+            borderCapStyle: 'butt',
+            borderDash: [],
+            borderDashOffset: 0.0,
+            borderJoinStyle: 'miter',
+            pointBorderColor: 'rgba(75,192,192,1)',
+            pointBackgroundColor: '#fff',
+            pointBorderWidth: 1,
+            pointHoverRadius: 5,
+            pointHoverBackgroundColor: 'rgba(75,192,192,1)',
+            pointHoverBorderColor: 'rgba(220,220,220,1)',
+            pointHoverBorderWidth: 2,
+            pointRadius: 1,
+            pointHitRadius: 10,
+            data: [65, 59, 80, 81, 56, 55, 45, 54, 72, 64, 88, 74],
+            spanGaps: false
+          }
+        ]
+      }
+    });
   }
 
   async segmentChanged() {
