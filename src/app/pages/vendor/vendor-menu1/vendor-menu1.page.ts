b500870ef2725d509fde63660724dba7bba8652d
nicolasmahlangu75 <nicolasmahlangu75@gmail.com>
2020-07-23 17:49:29 +0200

    Nicholas_progress

---
+++ src/app/pages/vendor/vendor-menu1/vendor-menu1.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-vendor-menu1',
+  templateUrl: './vendor-menu1.page.html',
+  styleUrls: ['./vendor-menu1.page.scss'],
+})
+export class VendorMenu1Page implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/pages/vendor/vendor-menu3/vendor-menu3.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-vendor-menu3',
+  templateUrl: './vendor-menu3.page.html',
+  styleUrls: ['./vendor-menu3.page.scss'],
+})
+export class VendorMenu3Page implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
