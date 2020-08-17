023a42c64c1cb29aa486a011ea7b499f0bc3b986
ashoktut <ashoktut.2@gmail.com>
2020-08-17 23:41:35 +0200

    S.o.S 2

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -174,7 +174,7 @@ export class HomePage implements OnInit {
   }
 
   gotoVendorMenu3() {
-    this.route.navigateByUrl('/order3');
+    this.route.navigateByUrl('/stripe-web');
   }
   // search bar
   // --------------------------------------------------
