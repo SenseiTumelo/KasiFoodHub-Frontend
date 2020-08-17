9e2e0049ad73905c8b26a385bf98667083834a32
ashoktut <ashoktut.2@gmail.com>
2020-08-17 23:46:31 +0200

    2.0 trial

---
+++ src/app/pages/stripe-web/stripe-web.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-stripe-web',
+  templateUrl: './stripe-web.page.html',
+  styleUrls: ['./stripe-web.page.scss'],
+})
+export class StripeWebPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
